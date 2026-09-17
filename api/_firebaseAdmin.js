5641dc95ca991f2cf24eeae472bc62948af5f7eb
Eyob <Eyobeb337@gmail.com>
2026-09-17 08:44:31 +0300

    Update _firebaseAdmin.js

--- api/_firebaseAdmin.js
+++ api/_firebaseAdmin.js
@@ -15,7 +15,6 @@ function getAdminApp() {
   const databaseURL = process.env.FIREBASE_DATABASE_URL;
 
   if (privateKey) {
- alert(privateKey);
     privateKey = privateKey.replace(/^['"]|['"]$/g, "").replace(/\\n/g, "\n");
   }
 
